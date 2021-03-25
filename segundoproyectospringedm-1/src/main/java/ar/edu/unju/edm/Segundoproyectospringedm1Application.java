8dffb987880c9a422afdbb9e45fb591210509eb4
Maxii <Maxii@DESKTOP-R4B6P3G>
2021-03-25 15:22:11 -0300

    hola

---
+++ segundoproyectospringedm-1/src/main/java/ar/edu/unju/edm/Segundoproyectospringedm1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Segundoproyectospringedm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Segundoproyectospringedm1Application.class, args);
+	}
+
+}
